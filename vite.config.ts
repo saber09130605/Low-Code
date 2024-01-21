a543eda69d7e430e2fddf08029f02bc3274c3fd8
saber09130605 <1031703364@qq.com>
2024-01-21 22:29:10 +0800

    @路径配置，pinia插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
+import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -14,4 +14,10 @@ export default defineConfig({
   Components({
     resolvers: [ElementPlusResolver()],
   })],
+  resolve:{
+    // 别名配置
+    alias:{
+      "@": path.resolve(__dirname, './src')
+    }
+  }
 })
